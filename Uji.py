6d1246adeb721b35446939fe75686b482a79cf10
mnga12 <60304556+mnga12@users.noreply.github.com>
2022-11-24 18:53:20 +0700

    Solovay-Strassen Primality Test.py

--- Uji.py
+++ Uji.py
@@ -1,7 +1,7 @@
 import random
 
 
-def kalikuadrat(basis, pangkat, mod):
+def kuadratkali(basis, pangkat, mod):  # algoritma kuadratkali, menghitung a^n dalam mod m
     x = 1
     y = basis
     while pangkat > 0:
@@ -12,47 +12,54 @@ def kalikuadrat(basis, pangkat, mod):
     return x
 
 
-def jacobian(a, n):  # jacobian udah bener
-    if a == 0:
-        return 0
+def jacobian(a, n):  # perhitungan simbol jacobi (a/n) dengan menggunakan sifat-sifat dari teori bliangan
     ans = 1
-    while a:
-        a = a % n
-        while a % 2 == 0:
+    while a:  # loop ini akan halt
+        a = a % n  # sifat 1 dari Rosen 3rd ed. hlm 183
+        if a == 1 or n == 1:
+            return ans
+        while a % 2 == 0 and a != 0:  # sifat 2 & 3
             a = a // 2
             if n % 8 == 3 or n % 8 == 5:
                 ans = -ans
-        if a % 4 == 3 and n % 4 == 3:  # sifat 4 tukar a dengan n, (a/n) = -(a/n) jika a=n=3 mod 4
+        if a % 4 == 3 and n % 4 == 3:  # sifat 4
             ans = -ans
+        if a == 0:
+            return 0
         a, n = n, a
-        if n == 1:
-            return ans
     return 0
 
 
 def solovoystrassen(p, k):
-    if p < 2:
+    if p < 2:  # jelas bilangan bulat kurang dari 2 bukan bilangan prima
         return False
-    if p != 2 and p % 2 == 0:
+    if p != 2 and p % 2 == 0:  # jika p kelipatan 2 dan tidak sama dengan 2 maka p bukan prima
         return False
     for i in range(k):
         # generate bilangan random diantara 1 sampai p-1
-        a = random.randrange(p - 1) + 1  # random.randrange(a) akan memberikan sebuah bilangan random bulat dari 0 sampai a-1.
-        hasil = jacobian(a, p)
-        mod = kalikuadrat(a, (p - 1)//2, p)
-        if p-mod == 1:
-            mod = -1
-        print(a,hasil,mod)
-        if hasil == 0 or mod != hasil:
+        a = random.randint(1, p-1)
+        jac = jacobian(a, p)
+        mod = kuadratkali(a, (p - 1) // 2, p)
+        if p - mod == 1:  # jika p-a = 1 maka a = -1 (mod p)
+            mod = -1  # dalam modulo p
+        if jac == 0 or mod != jac:  # teorema, jika p prima maka (a/p) = a^(p-1)/2 (mod p)
             return False
     return True
 
-def main():
-  n = int(input("n = "))
-  k = 100
-  if solovoystrassen(n,k):
-    print(n, "prima")
-  else:
-    print(n, "komposit")
 
-main()
+def test():  # untuk menguji suatu bilangan asli prima atau tidak
+    n = int(input("n = "))
+    k = 100
+    if solovoystrassen(n, k):
+        print(n, "mungkin prima")
+    else:
+        print(n, "komposit")
+
+
+def search():  # mencari bilangan prima pada suatu selang
+    a = int(input("batas bawah : "))
+    b = int(input("batas atas : "))
+    k = 100
+    for i in range(a-1, b):
+        if solovoystrassen(i, k):
+            print(i, "mungkin prima")
